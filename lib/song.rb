8e0b8e4a72742062c2508d04ca74bb40f7b1e999
Zoncharne Lee <zoncharne@gmail.com>
2019-03-22 06:50:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor 
+  
+  def initialize(name, artist, genre)
+    
+  
+  def count
+    
+  def artists
+    
+  def genre
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
