10b95a4eb1626280236b577988ea98db7e1e74c6
Zoncharne Lee <zoncharne@gmail.com>
2019-03-22 06:56:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,32 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    @@artist << artist
+    @@genres << genre
+  end
+  
+  def self.count
+  end
+  
+  def self.artists
+  end
+  
+  def self.genres
+  end
     
   
-  def count
-    
-  def artists
-    
-  def genre
-    
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   
   
   
